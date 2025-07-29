58fe007e55805e6d55462eb70866d36108a548f2
emon034 <sarkarmdemon361@gmail.com>
2025-07-28 21:19:08 -0700

    thired commit

--- src/commponent/hero/why-hobby.jsx
+++ src/commponent/hero/why-hobby.jsx
@@ -7,17 +7,29 @@ const WhyHobby = () => {
       <div className="mt-4 h-1 w-20 bg-accent mx-auto"></div>
         <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3 text-center mt-5">
           <div>
-            <h1></h1>
+            <h1>
+              <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" class="text-4xl mx-auto text-primary" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0z"></path><path d="M11 14H9a9 9 0 0 1 9-9v2c-3.87 0-7 3.13-7 7zm7-3V9c-2.76 0-5 2.24-5 5h2c0-1.66 1.34-3 3-3zM7 4c0-1.11-.89-2-2-2s-2 .89-2 2 .89 2 2 2 2-.89 2-2zm4.45.5h-2A2.99 2.99 0 0 1 6.5 7h-3C2.67 7 2 7.67 2 8.5V11h6V8.74a4.97 4.97 0 0 0 3.45-4.24zM19 17c1.11 0 2-.89 2-2s-.89-2-2-2-2 .89-2 2 .89 2 2 2zm1.5 1h-3a2.99 2.99 0 0 1-2.95-2.5h-2A4.97 4.97 0 0 0 16 19.74V22h6v-2.5c0-.83-.67-1.5-1.5-1.5z">
+                </path>
+                </svg>
+            </h1>
             <h4 className="text-xl font-semibold mt-2">Connect Locally</h4>
             <p>Meet people nearby who share your hobbies and interests.</p>
           </div>
           <div>
-             <h1></h1>
+             <h1>
+              <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" class="text-4xl mx-auto text-primary" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M9 20h6v2H9zm7.906-6.288C17.936 12.506 19 11.259 19 9c0-3.859-3.141-7-7-7S5 5.141 5 9c0 2.285 1.067 3.528 2.101 4.73.358.418.729.851 1.084 1.349.144.206.38.996.591 1.921H8v2h8v-2h-.774c.213-.927.45-1.719.593-1.925.352-.503.726-.94 1.087-1.363zm-2.724.213c-.434.617-.796 2.075-1.006 3.075h-2.351c-.209-1.002-.572-2.463-1.011-3.08a20.502 20.502 0 0 0-1.196-1.492C7.644 11.294 7 10.544 7 9c0-2.757 2.243-5 5-5s5 2.243 5 5c0 1.521-.643 2.274-1.615 3.413-.373.438-.796.933-1.203 1.512z">
+                </path>
+                </svg>
+             </h1>
             <h4 className="text-xl font-semibold mt-2">Learn Together</h4>
             <p>Grow skills through group activities, projects, and meetups.</p>
           </div>
           <div>
-            <h1></h1>
+            <h1>
+             <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" class="text-4xl mx-auto text-primary" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M288 421a48 48 0 1 0 96 0 48 48 0 1 0-96 0zm352 0a48 48 0 1 0 96 0 48 48 0 1 0-96 0zM512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm263 711c-34.2 34.2-74 61-118.3 79.8C611 874.2 562.3 884 512 884c-50.3 0-99-9.8-144.8-29.2A370.4 370.4 0 0 1 248.9 775c-34.2-34.2-61-74-79.8-118.3C149.8 611 140 562.3 140 512s9.8-99 29.2-144.8A370.4 370.4 0 0 1 249 248.9c34.2-34.2 74-61 118.3-79.8C413 149.8 461.7 140 512 140c50.3 0 99 9.8 144.8 29.2A370.4 370.4 0 0 1 775.1 249c34.2 34.2 61 74 79.8 118.3C874.2 413 884 461.7 884 512s-9.8 99-29.2 144.8A368.89 368.89 0 0 1 775 775zM664 533h-48.1c-4.2 0-7.8 3.2-8.1 7.4C604 589.9 562.5 629 512 629s-92.1-39.1-95.8-88.6c-.3-4.2-3.9-7.4-8.1-7.4H360a8 8 0 0 0-8 8.4c4.4 84.3 74.5 151.6 160 151.6s155.6-67.3 160-151.6a8 8 0 0 0-8-8.4z">
+              </path>
+              </svg>
+            </h1>
             <h4 className="text-xl font-semibold mt-2">Enjoy Your Time</h4>
             <p>Reclaim your free time with fun and meaningful hobbies.</p>
           </div>
--- src/commponent/users/testimonials.jsx
+++ src/commponent/users/testimonials.jsx
@@ -9,33 +9,34 @@ const Testimonials = () => {
       <h2 className="text-3xl md:text-4xl font-bold text-center mb-8">🌟 What users say</h2>
         <div className="mt-4 h-1 w-20 bg-accent mx-auto"></div>
         <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3 items-center text-center mt-5">
-            <div className="p-6 bg-white rounded-lg shadow-md">
-                <p className="text-gray-700">"HobbyHub has transformed my free time! I've met amazing people and learned so much."</p>
+            <div className="p-6  rounded-lg shadow-md">
+                <p className="">"HobbyHub has transformed my free time! I've met amazing people and learned so much."</p>
                <img className="w-20 h-20 rounded-full border-2  text-center " src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5mtLfjCs0GKjf_hu2VELxYI6tG3OtljyAxg&s" alt="" />
                 <h4 className="mt-4 font-semibold">- Sarah L.</h4>
             </div>
-            <div className="p-6 bg-white rounded-lg shadow-md">
-                <p className="text-gray-700">"The community is so supportive. I love sharing my photography passion with others."</p>
+            <div className="p-6  rounded-lg shadow-md">
+                <p className="">"The community is so supportive. I love sharing my photography passion with others."</p>
                  <img className="w-20 h-20 rounded-full border-2  i " src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlDCzpqMjGM2CdBfLU7m_pLXvhQn1i6_1q0w&s" alt="" />
-                <h4 className="mt-4 font-semibold">- Mike D.</h4>
+                <h4 className="mt-4 font-semibold
+                ">- Mike D.</h4>
             </div>
-            <div className="p-6 bg-white rounded-lg shadow-md text-center">
-                <p className="text-gray-700">"I never knew how much I needed a hobby group until I joined HobbyHub. It's been a game-changer!"</p>
+            <div className="p-6 rounded-lg shadow-md text-center">
+                <p className="">"I never knew how much I needed a hobby group until I joined HobbyHub. It's been a game-changer!"</p>
                  <img className="w-20 h-20 rounded-full border-2  text-center " src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrK5uVVmJ5CJ1SZlzYnKsCFIlEQRMV3AzaoA&s" alt="" />
                 <h4 className="mt-4 font-semibold">- Emily R.</h4>
             </div>
-            <div className="p-6 bg-white rounded-lg shadow-md">
-                <p className="text-gray-700">"HobbyHub helped me discover my love for painting. The workshops are fantastic!"</p>
+            <div className="p-6  rounded-lg shadow-md">
+                <p className="">"HobbyHub helped me discover my love for painting. The workshops are fantastic!"</p>
                 <img className="w-20 h-20 rounded-full border-2  text-center " src="https://wevote-images.s3.amazonaws.com/wv87cand3102350/twitter_profile_image-20240702_1_200x200.jpeg" alt="" />
                 <h4 className="mt-4 font-semibold">- John S.</h4>
             </div>
-            <div className="p-6 bg-white rounded-lg shadow-md">
-                <p className="text-gray-700">"I joined a hiking group through HobbyHub and now I have friends to explore the outdoors with!"</p>
+            <div className="p-6  rounded-lg shadow-md">
+                <p className="">"I joined a hiking group through HobbyHub and now I have friends to explore the outdoors with!"</p>
                 <img className="w-20 h-20 rounded-full border-2  text-center " src="https://cdn.i-scmp.com/sites/default/files/styles/768x768/public/d8/images/methode/2019/03/27/dffa4156-4f80-11e9-8617-6babbcfb60eb_image_hires_141554.JPG?itok=FNC2TjNJ&v=1553667358" alt="" />
                 <h4 className="mt-4 font-semibold">- Lisa K.</h4>
             </div>
-            <div className="p-6 bg-white rounded-lg shadow-md">
-                <p className="text-gray-700">"HobbyHub is more than just a platform; it's a community that inspires and motivates me every day."</p>
+            <div className="p-6  rounded-lg shadow-md">
+                <p className="">"HobbyHub is more than just a platform; it's a community that inspires and motivates me every ."</p>
                 <img className="w-20 h-20 rounded-full border-2  text-center " src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQk12g33EWr9UhVmCpmvZoJRs9XQq9JzxZ_JA&s" alt="" />
                 <h4 className="mt-4 font-semibold">- David T.</h4>
             </div>
